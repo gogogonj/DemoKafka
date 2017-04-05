a6f665399a8ecd02af006cf0348757e8ad007c96
wangwei_yx <wangwei@asiainfo.com>
2017-04-05 15:29:43 +0800

    Kafka Java API操作topic

---
+++ src/main/java/topics/TopicAPITest.java
@@ -0,0 +1,20 @@
+package topics;
+
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
+import kafka.utils.ZkUtils;
+import org.apache.kafka.common.security.JaasUtils;
+
+import java.util.Properties;
+
+/**
+ * Created by AI on 2017/4/5.
+ */
+public class TopicAPITest {
+    public static void main(String [] args){
+        ZkUtils zkUtils = ZkUtils.apply("localhost:2181", 30000, 30000, JaasUtils.isZkSecurityEnabled());
+        // 创建一个单分区单副本名为t1的topic
+        AdminUtils.createTopic(zkUtils, "t1", 1, 1, new Properties(), RackAwareMode.Enforced$.MODULE$);
+        zkUtils.close();
+    }
+}
